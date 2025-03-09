201b315b4068bf135109b2fb9241c2328495dbca
gladwin8 <isaac.gladwin8@gmail.com>
2025-03-09 23:58:02 +0530

    Traditional switch & enhanced switch

--- src/main/java/org/basics/Switch_case.java
+++ src/main/java/org/basics/Switch_case.java
@@ -5,6 +5,11 @@ public class Switch_case {
     public static void main(String[] args) {
 
         int value = 10;
+        traditional_switch(value);
+        enhanced_switch(value);
+    }
+
+    public static void enhanced_switch(int value) {
         switch(value){
             case 4:
                 System.out.println("Sergio Ramos");
@@ -15,9 +20,27 @@ public class Switch_case {
             case 8: case 10:
                 System.out.println("Iniesta or Kroos or Modric or Messi !!");
                 System.out.println("Jersey Number is " +value);
+                break;
             default:
                 System.out.println("Great Football Talents !!");
         }
+    }
 
+    public static void traditional_switch(int value)
+    {
+        switch(value){
+            case 4:
+                System.out.println("Sergio Ramos");
+                break;
+            case 7:
+                System.out.println("Cristiano Ronaldo");
+                break;
+            case 8: case 10:
+                System.out.println("Iniesta or Kroos or Modric or Messi !!");
+                System.out.println("Jersey Number is " +value);
+                break;
+            default:
+                System.out.println("Great Football Talents !!");
+        }
     }
 }
